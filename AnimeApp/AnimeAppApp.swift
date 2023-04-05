c7583ef4323b39d38436dc3d2cf7e38fea538d2e
Javier Etxarri <javieretxarrimarin@gmail.com>
2023-04-05 11:42:03 +0200

    Initial Commit

---
+++ AnimeApp/AnimeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimeAppApp.swift
+//  AnimeApp
+//
+//  Created by Javier Etxarri on 5/4/23.
+//
+
+import SwiftUI
+
+@main
+struct AnimeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
